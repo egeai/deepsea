7eed963ea138a803087bf4d2f2011628a591be7e
Bertan Ulusoy <bertan_ulusoy@yahoo.com>
2023-07-26 15:34:42 +0300

    Cats_vs_Dogs binary classification model created.

---
+++ src/deepsea/network_architecture/classification/image/basic/cats_vs_dogs/convnet_model.py
@@ -0,0 +1,21 @@
+from tensorflow import keras
+from tensorflow.keras import layers
+
+def model():
+    inputs = keras.Input(shape=(180, 180, 3))
+    x = layers.Rescaling(1./255)(inputs)
+    x = layers.Conv2D(filters=32, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=64, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=128, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=256, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPooling2D(pool_size=2)(x)
+    x = layers.Conv2D(filters=256, kernel_size=3, activation="relu")(x)
+    x = layers.Flatten()(x)
+    outputs = layers.Dense(1, activation="sigmoid")(x)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+
+    return model
+
