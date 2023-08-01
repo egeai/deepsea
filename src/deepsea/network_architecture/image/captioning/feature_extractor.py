f860619ba63c8fdc04e97e3ad5e91320dbd43b9c
Bertan Ulusoy <bertan_ulusoy@yahoo.com>
2023-08-01 23:25:46 +0300

    feature extractor updated..

--- src/deepsea/network_architecture/image/captioning/feature_extractor.py
+++ src/deepsea/network_architecture/image/captioning/feature_extractor.py
@@ -5,11 +5,7 @@ import pickle
 from string import punctuation
 import numpy as np
 import tqdm
-
-import tensorflow
-from tensorflow.keras import Input
-from keras.utils import load_img, img_to_array
-from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow import keras
 from tensorflow.keras.applications.vgg16 import *
 from tensorflow.keras.layers import *
 from tensorflow.keras.preprocessing.image import *
@@ -20,40 +16,38 @@ from tqdm import tqdm
 
 
 class ImageCaptionFeatureExtractor(object):
-    def __init__(self, output_path,
-                 start_token='beginsequence',
-                 end_token='endsequence',
-                 feature_extractor=None,
-                 input_shape=(224, 224, 3)):
-
+    def __init__(
+        self,
+        output_path,
+        start_token="beginsequence",
+        end_token="endsequence",
+        feature_extractor=None,
+        input_shape=(224, 224, 3),
+    ):
         self.input_shape = input_shape
-
         if feature_extractor is None:
-            input = Input(shape=input_shape)
-            self.feature_extractor = VGG16(
-                input_tensor=input,
-                weights='imagenet',
-                include_top=False
+            input = keras.Input(shape=input_shape)
+            self.feature_extractor = keras.applications.vgg16.VGG16(
+                input_tensor=input, weights="imagenet", include_top=False
             )
         else:
-        self.feature_extractor = feature_extractor
-        self.output_path = output_path
-        self.start_token = start_token
-        self.end_token = end_token
-        self.tokenizer = Tokenizer()
-        self.max_seq_length = None
+            self.feature_extractor = feature_extractor
+            self.output_path = output_path
+            self.start_token = start_token
+            self.end_token = end_token
+            self.tokenizer = Tokenizer()
+            self.max_seq_length = None
 
     def extract_image_features(self, image_path):
-        image = load_img(image_path,
-                         target_size=self.input_shape[:2])
-        image = img_to_array(image)
+        image = keras.utils.load_img(image_path, target_size=self.input_shape[:2])
+        image = keras.utils.img_to_array(image)
         image = np.expand_dims(image, axis=0)
-        image = preprocess_input(image)
+        image = keras.applications.vgg16.preprocess_input(image)
         return self.feature_extractor.predict(image)[0]
 
     def _clean_captions(self, captions):
         def remove_punctuation(word):
-            translation = str.maketrans('', '', punctuation)
+            translation = str.maketrans("", "", punctuation)
             return word.translate(translation)
 
         def is_valid_word(word):
@@ -61,21 +55,20 @@ class ImageCaptionFeatureExtractor(object):
 
         cleaned_captions = []
         for caption in captions:
-            caption = caption.lower().split(' ')
+            caption = caption.lower().split(" ")
             caption = map(remove_punctuation, caption)
             caption = filter(is_valid_word, caption)
-            cleaned_caption = f'{self.start_token} ' \
-                               f'{" ".join(caption)} ' \
-                                f'{self.end_token}'
+            cleaned_caption = (
+                f"{self.start_token} " f'{" ".join(caption)} ' f"{self.end_token}"
+            )
             cleaned_captions.append(cleaned_caption)
         return cleaned_captions
 
     def _get_max_seq_length(self, captions):
         max_sequence_length = -1
         for caption in captions:
-            caption_length = len(caption.split(' '))
-            max_sequence_length = max(max_sequence_length,
-                                      caption_length)
+            caption_length = len(caption.split(" "))
+            max_sequence_length = max(max_sequence_length, caption_length)
         return max_sequence_length
 
     def extract_features(self, images_path, captions):
@@ -86,22 +79,19 @@ class ImageCaptionFeatureExtractor(object):
         self.tokenizer.fit_on_texts(captions)
 
         data_mapping = {}
-        print('\nExtracting features...')
+        print("\nExtracting features...")
         for i in tqdm(range(len(images_path))):
             image_path = images_path[i]
             caption = captions[i]
             feats = self.extract_image_features(image_path)
 
             image_id = image_path.split(os.path.sep)[-1]
-            image_id = image_id.split('.')[0]
+            image_id = image_id.split(".")[0]
 
-            data_mapping[image_id]  = {
-                'features': feats,
-                'caption': caption
-            }
+            data_mapping[image_id] = {"features": feats, "caption": caption}
 
-            out_path = f'{self.output_path}/data_mapping.pickle'
-            with open(out_path, 'wb') as f:
+            out_path = f"{self.output_path}/data_mapping.pickle"
+            with open(out_path, "wb") as f:
                 pickle.dump(data_mapping, f, protocol=4)
 
             self._create_sequences(data_mapping)
@@ -113,6 +103,10 @@ class ImageCaptionFeatureExtractor(object):
         in_seqs = []
         out_seqs = []
 
-        print('\nCreating sequences...')
-
-
+        print("\nCreating sequences...")
+        # iterate over each features-caption pair.
+        for _, data in tqdm(mapping.items()):
+            feature = data["features"]
+            caption = data["caption"]
+            seq = self.tokenizer.texts_to_sequences([caption])
+            seq = seq[0]
